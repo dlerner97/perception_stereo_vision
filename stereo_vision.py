6fc9ca25f51d0254a99c47ead7906510f581d163
Dani Lerner <dlerner97@gmail.com>
2021-04-25 16:29:18 -0400

    Working depth map, needs parameter adjustment

--- stereo_vision.py
+++ stereo_vision.py
@@ -1,3 +1,4 @@
+# =========================== Imports =========================== #
 import cv2
 import json
 import numpy as np
@@ -7,29 +8,44 @@ from numpy.lib.function_base import disp
 from scipy.signal import convolve2d
 from video_handler import VideoHandler
 import matplotlib.pyplot as plt
+from pandas import DataFrame
 
+# =========================== StereoVision Class =========================== #
 class StereoVision:
+    
+    """
+    Stereo Vision Class Declaration
+    
+    args: Camera paremeters dict, left image, right image
+    
+    The Stereo Vision class takes two images along with some camera parameters to calculate the approximate depth of objects in an image. Returns a heat map. 
+    """
+    
     def __init__(self, cam_params, img0, img1) -> None:
         self.img0 = img0
         self.img1 = img1
         self.cam_params = cam_params
         self.img_count = 0
 
-    def get_matches(self, img0=None, img1=None, num_matches=50, debug=False):
+    # Get keypoint matches with SIFT
+    def get_matches(self, img0=None, img1=None, num_matches=70, debug=False):
         if img0 is None:
             img0 = self.img0
         if img1 is None:
             img1 = self.img1
         
+        # Find keypoints
         sift = cv2.SIFT_create()
         
         keypts_0, hog_0 = sift.detectAndCompute(img0, None)
         keypts_1, hog_1 = sift.detectAndCompute(img1, None)  
         
+        # Match keypoints between the two images
         bf = cv2.BFMatcher(cv2.NORM_L1, crossCheck=True)
         matches = bf.match(hog_0, hog_1)
         matches = sorted(matches, key = lambda x: x.distance)
         
+        # Display matching
         if debug:
             match_img = cv2.drawMatches(img0, keypts_0, img1, keypts_1, matches[:num_matches], img1, flags=2)
             cv2.imshow(f"img{self.img_count}", cv2.drawKeypoints(img0, keypts_0, img0))
@@ -39,16 +55,20 @@ class StereoVision:
             cv2.imshow("matches", match_img)
             cv2.waitKey(0)
          
+        # Return desired number of matches 
         if num_matches <= 0:
             return matches, keypts_0, keypts_1  
         return matches[:num_matches], keypts_0, keypts_1
     
+    # Calculate the Fundametal and Essential matrices determined from keypoint matching
     def get_cameras_rel(self, match_info):
         matches, keypts_0, keypts_1 = match_info
         
         A = []
         self.sorted_keypts_0 = []
         self.sorted_keypts_1 = []
+        
+        # Build A matrix
         for match in matches:
             u, v = keypts_0[match.queryIdx].pt
             u_p, v_p = keypts_1[match.trainIdx].pt
@@ -64,23 +84,27 @@ class StereoVision:
         
         # F, self.mask = cv2.findFundamentalMat(self.sorted_keypts_0, self.sorted_keypts_1, cv2.RANSAC)
         
+        # Use SVD to calculate the Fundamental Matrix
         _, _, V = np.linalg.svd(A)
         F = V[-1, :]
         F = (F/F[-1]).reshape(3,3)
         
+        # Use camera paremeters and fundamental matrix to calculate essential matrix
         self.K0 = np.array(self.cam_params["cam0"])
         self.K1 = np.array(self.cam_params["cam1"])  
         E = self.K1.T @ F @ self.K0
         E = E/E[-1, -1]
         
+        # Calculate camera rotation and translation with SVD of essential matrix
         U, S, V = np.linalg.svd(E)
         W = np.array([[0,-1,0], [1,0,0], [0,0,1]])
         t = U @ W @ S @ U.T
         R = U @ W.T @ V.T
         return R, t, F
       
-    def rectify_images(self):
-        matches, keypts_0, keypts_1 = self.get_matches()
+    # Rectify the images such that every epipolar line is horizontal  
+    def rectify_images(self, debug=False):
+        matches, keypts_0, keypts_1 = self.get_matches(debug=debug)
         R, t, F = self.get_cameras_rel((matches, keypts_0, keypts_1))
         _, H0, H1 = cv2.stereoRectifyUncalibrated(self.sorted_keypts_0, self.sorted_keypts_1, F, self.img0.shape[:2][::-1])
         print(10*"-", "H0", 10*"-")
@@ -91,30 +115,32 @@ class StereoVision:
         print(H1)
         print(24*"-")
         
-        # self.disp_epipoles(self.img0, self.img1, F)
+        if debug:
+            self.disp_epipoles(self.img0, self.img1, F)
+           
+        # Recalculate F and keypoint locations while accounting for rectification
         F = (np.linalg.inv(H1)).T @ F @ np.linalg.inv(H0)
         keypts_0 = np.array([self.sorted_keypts_0], dtype=np.float32)
         keypts_1 = np.array([self.sorted_keypts_1], dtype=np.float32)
         self.sorted_keypts_0 = cv2.perspectiveTransform(keypts_0, H0)[0].astype(np.int32, copy=False)
         self.sorted_keypts_1 = cv2.perspectiveTransform(keypts_1, H1)[0].astype(np.int32, copy=False)
         
+        # Rectify images
         img0 = cv2.warpPerspective(self.img0, H0, self.img0.shape[:2][::-1])
         img1 = cv2.warpPerspective(self.img1, H1, self.img1.shape[:2][::-1])
-        # self.disp_epipoles(img0, img1, F)
+        
+        # Display epipolar lines
+        if debug:
+            self.disp_epipoles(img0, img1, F)
+            
         return img0, img1, F
         
-    def apply_window_matching(self, window_size=19, disparity_thresh=500, debug=False):
-        img0, img1, F = self.rectify_images()
-        epilines0 = cv2.computeCorrespondEpilines(self.sorted_keypts_0.reshape(-1,1,2), 2, F).reshape(-1,3)
-        y_vals_0 = [int(round(-line_wt[2]/line_wt[1])) for line_wt in epilines0]
-        y_vals_0.sort()
-        
-        epilines1 = cv2.computeCorrespondEpilines(self.sorted_keypts_1.reshape(-1,1,2), 2, F).reshape(-1,3)
-        y_vals_1 = [int(round(-line_wt[2]/line_wt[1])) for line_wt in epilines1]
-        y_vals_1.sort()
-        
+    # Apply window matching to left and right images to get disparity
+    def apply_window_matching(self, window_size=11, debug=False):
+        img0, img1, F = self.rectify_images(debug=debug)       
         win_r = window_size//2
                
+        # Blur image
         img0_gray = cv2.GaussianBlur(cv2.cvtColor(img0, cv2.COLOR_BGR2GRAY), (15, 15), 0)
         img1_gray = cv2.GaussianBlur(cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY), (15, 15), 0)
         s0 = img0_gray.shape[1]
@@ -124,19 +150,30 @@ class StereoVision:
         disparity = []
         wait = 1
         sy = min([img0_gray.shape[0], img0_gray.shape[1]])
-        print("0%")
+        print('[' + 50*' '+"] 0%")
+        
+        # Apply window matching
         for y in range(win_r+1, sy-(win_r+1+1)):
             disparity.append([])
             for px0 in range(win_r, s0-(win_r+1)):
 
+                # The "kernel" is the window on the left hand image we are currently inspecting.
+                # Note, the kernel is flipped since we will be running convolution on the image.
                 kernel = img0_gray[y-win_r:y+win_r+1, px0-win_r:px0+win_r+1].astype(int)[::-1, ::-1]
+                
+                # Normalize the kernel
                 kernel = (kernel - np.mean(kernel))/(np.std(kernel)+.01)
 
+                # The image segment is the temporary image we will convolve the kernel with. 
+                # This image segment is the entire width of the right-hand image and enough rows to check the same line as the other image along with a row below and row above
                 img_segment = img1_gray[y-win_r-1:y+win_r+1+1, :]
+                
+                # Normalize the image
                 seg_mean = np.mean(img_segment)
                 seg_std = np.std(img_segment)
-                
                 img_segment = (img_segment - seg_mean)/(seg_std+.01)
+                
+                # Convolve the kernel and image to find the maximum normalized correlation 
                 res = convolve2d(img_segment, kernel, mode='valid')
                 px1 = (np.argmax(res) + win_r + 1)%s0
                 
@@ -166,50 +203,51 @@ class StereoVision:
                         plt.plot(res)
                         plt.show()
 
-            print ("\033[A                             \033[A")
+            print ("\033[A                                          \033[A")
             per_done = 100*y/(sy-(win_r+1))
-            print(round(per_done/2)*"|", "  ", f"{round(per_done, 2)}%")
+            print('[' + round(per_done/2)*"|", (46-round(per_done/2))*" " + ']', f" {round(per_done, 2)}%")
            
         print ("\033[A                             \033[A")
-        print(50*"|", "  ", "100% Finished!")
-        
-        if debug:
-            from pandas import DataFrame
-            print(DataFrame(disparity))
-        
-        disparity = np.array(disparity)
-        min_corr = np.min(disparity)
-        max_corr = np.max(disparity)
-        disparity_map = np.round(255-255*(disparity.ravel() - min_corr)/(max_corr - min_corr)).astype(np.uint8).reshape(disparity.shape)
-        cv2.imshow("disparity map", disparity_map)
-        
-        disparity[disparity > disparity_thresh] = disparity_thresh
+        print('[' + 46*"|", "  ", "] 100% Finished!")
         
         if debug:
             print(DataFrame(disparity))
+        
+        return np.array(disparity)
+
+    def get_depth_map(self, disparity, disparity_thresh=400, depth_thresh=7000, debug=False):
+        self.disp_heat_map(disparity, "disparity")
+        
+        plt.hist(disparity.ravel(), 256, [min(disparity.ravel()),max(disparity.ravel())])
+        plt.show()
+        
+        disparity_thresholded = disparity.copy()
+        disparity_thresholded[disparity_thresholded > disparity_thresh] = disparity_thresh
+        self.disp_heat_map(disparity_thresholded, "disparity thresholded")
             
-        min_corr = np.min(disparity)
-        max_corr = np.max(disparity)
-        disparity_map_thresh = np.round(255-255*(disparity.ravel() - min_corr)/(max_corr - min_corr)).astype(np.uint8).reshape(disparity.shape)
-        cv2.imshow("thresholded disparity map", disparity_map_thresh)
-        
-        self.disp_heat_map(disparity_map)
-        self.disp_heat_map(disparity_map_thresh)
-        
         baseline = self.cam_params["baseline"]
         focal_len = self.cam_params["cam0"][0][0]
-        print(focal_len)
+        bf = baseline*focal_len
         
+        depth = bf/disparity_thresholded
+        depth[depth > depth_thresh] = depth_thresh
+        print(depth)
+        self.disp_heat_map(depth, "depth")
         
         cv2.waitKey(0)
-        
-                 
-    def disp_heat_map(self, img):        
+                      
+    def disp_heat_map(self, img_like, name):  
+        min_val = np.min(img_like)
+        max_val = np.max(img_like)
+        img_map = np.round(255-255*((img_like.ravel() - min_val)/(max_val - min_val + 1))**1.6).astype(np.uint8).reshape(img_like.shape)
+        cv2.imshow(f"{name} gray map", img_map)
+        cv2.imwrite(f"{name}_gray_map.png", img_map)
+              
         colormap = plt.get_cmap('jet')
-        heatmap = (colormap(img) * 2**16).astype(np.uint16)
+        heatmap = (colormap(img_map) * 2**16).astype(np.uint16)
         heatmap = cv2.cvtColor(heatmap, cv2.COLOR_RGB2BGR)
-        cv2.imshow(f"heat map {self.img_count}", heatmap)
-        self.img_count += 1
+        cv2.imshow(f"{name} heat map", heatmap)
+        cv2.imwrite(f"{name}_heat_map.png", img_map)
 
     def disp_epipoles(self, img0, img1, F):   
         img0 = img0.copy()
@@ -254,7 +292,8 @@ if __name__ == "__main__":
     img1 = VideoHandler.resize_frame(img1, 25)
    
     stereo_vis = StereoVision(cam_params[f"d{ds}"], img0, img1)
-    stereo_vis.apply_window_matching()
+    disparity = stereo_vis.apply_window_matching()
+    stereo_vis.get_depth_map(disparity)
     
     # Calculate time elapsed
     time_elapsed_s = time() - start_time
